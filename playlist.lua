bccceddfd569c690784efb0b8fe7fe1dd9124315
Misyurov Artemiy <misyurov_aa@ekb.sima-land.ru>
2022-07-04 15:21:09 +0500

    color to next album in playlist

--- playlist.lua
+++ playlist.lua
@@ -4,74 +4,78 @@ function player()
     end
 end
 
+---
 function mopidy_player()
     local current_t_a_al_d = trim(read_CLI('mpc current -f %title%💩%artist%💩%album%💩%date%'))
     if string.len(current_t_a_al_d) > 0 then
-        local current,artist,album,date = current_t_a_al_d:match('(.*)💩(.*)💩(.*)💩(%d*)')
-        local tt_ct_p_tc_pt = read_CLI('mpc status %totaltime%💩%currenttime%💩%songpos%💩%length%💩%percenttime%')
-        local tt_m,tt_s,ct_m,ct_s,current_num,total_count,_,pt = tt_ct_p_tc_pt:match('(%d+):(%d+)💩(%d+):(%d+)💩(%d+)💩(%d+)💩(%s+)(%d+)')
-        if tt_m == nil then tt_m, tt_s, ct_m, ct_s, total_count = 0, 0, 0, 0, 0 end
-        local u_el_time = (tt_m * 60 + tt_s) - (ct_m * 60 + ct_s)
-        local el_time   = os.date("%M:%S", u_el_time)
-        local count = 5
-        current_num = tonumber(current_num)
-        local start = tonumber(total_count - count + 1)
-        if total_count - current_num >= count then
-            start = current_num
-        end
-        local stop = start + count
-        local el_total_time = 0
-        local y_start = 635
-        local playlist = split(read_CLI('mpc playlist -f %title%💩%time%'), '\n')
-        for N in pairs(playlist) do
-            local song, time_m, time_s = playlist[N]:match('(.*)💩(%d+):(%d+)')
-            if N > current_num then
-                el_total_time = el_total_time + time_m * 60 + time_s
+        local tt_ct_p_tc_pt = read_CLI('mpc status %state%💩%totaltime%💩%currenttime%💩%songpos%💩%length%💩%percenttime%')
+        local state,tt_m,tt_s,ct_m,ct_s,current_num,total_count,_,pt = tt_ct_p_tc_pt:match('(%a+)💩(%d+):(%d+)💩(%d+):(%d+)💩(%d+)💩(%d+)💩(%s+)(%d+)')
+        if state == 'playing' then
+            local current,artist,album,date = current_t_a_al_d:match('(.*)💩(.*)💩(.*)💩(%d*)')
+            if tt_m == nil then tt_m, tt_s, ct_m, ct_s, total_count = 0, 0, 0, 0, 0 end
+            local u_el_time = (tt_m * 60 + tt_s) - (ct_m * 60 + ct_s)
+            local el_time   = os.date("%M:%S", u_el_time)
+            local count = 5
+            current_num = tonumber(current_num)
+            local start = tonumber(total_count - count + 1)
+            if total_count - current_num >= count then
+                start = current_num
             end
-            if start <= N and N < stop then
-                local y_step = 18
-                local color = def.color
-                local song_time = time_m .. ':' .. time_s
-                if song == current then
-                    el_total_time = el_total_time + u_el_time
-                    color = '0x3daee9'
-                    song_time = string.gsub('-'..el_time, "-0", "-")
+            local stop = start + count
+            local el_total_time = 0
+            local y_start = 635
+            local playlist = split(read_CLI('mpc playlist -f %title%💩%time%💩%album%'), '\n')
+            for N in pairs(playlist) do
+                local song, time_m, time_s, pl_album = playlist[N]:match('(.*)💩(%d+):(%d+)💩(.*)')
+                if N > current_num then
+                    el_total_time = el_total_time + time_m * 60 + time_s
                 end
-                song = string.gsub(song, '.wav', '')
-                text_by_left ({x=53, y=y_start}, trim(song), {color=color}, { width=250, col=1 })
-                text_by_right({x=313, y=y_start}, song_time, {color=color})
-                y_start = y_start + y_step
+                if start <= N and N < stop then
+                    local y_step = 18
+                    local color = def.color
+                    if pl_album ~= album then color = '0x666666' end
+                    local song_time = time_m .. ':' .. time_s
+                    if song == current then
+                        el_total_time = el_total_time + u_el_time
+                        color = '0x3daee9'
+                        song_time = string.gsub('-'..el_time, "-0", "-")
+                    end
+                    song = string.gsub(song, '.wav', '')
+                    text_by_left ({x=53, y=y_start}, trim(song), {color=color}, { width=250, col=1 })
+                    text_by_right({x=313, y=y_start}, song_time, {color=color})
+                    y_start = y_start + y_step
+                end
             end
-        end
 
-        local total_time
-        if el_total_time >= 3600 then
-            total_time = os.date("-%X", el_total_time-5*60*60)
-        else
-            total_time = os.date("-%M:%S", el_total_time)
-        end
-        if string.len(date) > 0 then date = ' ('..date..')' else date = '' end
-        draw_dash_bar({
-            height = 7,
-            width = 310,
-            seg_width = 3,
-            seg_margin = 3,
-            start_x = 4,
-            y = 612,
-            value = tonumber(pt),
-            colors = {
-                { color = '0x3daee9', alpha = 1 },
-                { color = def.color, alpha = .3 },
-            }
-        })
-        album = cut_string(album, 28, '…')
-        text_by_left  ({x=5, y=600}, artist, { weight = weight_bold })
-        text_by_right ({x=313, y=600}, album..date)
-        display_image ({ coord = { x = 5, y = 625 }, img = '/tmp/album_cover.png'} )
-        text_by_center( {x=23, y=685}, current_num..'/'..total_count )
-        text_by_center( {x=23, y=701}, string.gsub(total_time, "-0", "-"), {} )
+            local total_time
+            if el_total_time >= 3600 then
+                total_time = os.date("-%X", el_total_time-5*60*60)
+            else
+                total_time = os.date("-%M:%S", el_total_time)
+            end
+            if string.len(date) > 0 then date = ' ('..date..')' else date = '' end
+            draw_dash_bar({
+                height = 7,
+                width = 310,
+                seg_width = 3,
+                seg_margin = 3,
+                start_x = 4,
+                y = 612,
+                value = tonumber(pt),
+                colors = {
+                    { color = '0x3daee9', alpha = 1 },
+                    { color = def.color, alpha = .3 },
+                }
+            })
+            --album = cut_string(album, 28, '…')
+            text_by_left  ({x=5, y=600}, artist, { weight = weight_bold })
+            text_by_right ({x=313, y=600}, album..date, { width=200, col=1 })
+            display_image ({ coord = { x = 5, y = 625 }, img = '/tmp/album_cover.png'} )
+            text_by_center( {x=23, y=685}, current_num..'/'..total_count )
+            text_by_center( {x=23, y=701}, string.gsub(total_time, "-0", "-"), {} )
 
-        return true
+            return true
+        end
     end
 
     return false
@@ -132,7 +136,7 @@ function draw_player(icon, title, total_time, playing_time, el_time, artist, med
         }
     })
     text_by_left ({x=5, y=627}, artist, { size=13 })
-    text_by_left ({x=55, y=663}, title, { size=13 },{width = 220, margin=15})
+    text_by_left ({x=55, y=663}, title, { size=13 },{width = 210, margin=15})
     text_by_right( {x=313, y=663}, '-'..el_time)
     if img ~= nil and string.len(img) > 0 then
         get_img(mediaSrc, img)
